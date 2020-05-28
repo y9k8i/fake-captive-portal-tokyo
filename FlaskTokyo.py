2837a37c69977081cda7e38a6ca09afe65f867d4
y9k8i <28494556+y9k8i@users.noreply.github.com>
2020-05-29 08:36:45 +0900

    :wrench: upgrade version to 1.0

--- FlaskTokyo.py
+++ FlaskTokyo.py
@@ -5,9 +5,9 @@ import wifipumpkin3.core.utility.constants as C
 class FlaskTokyo(CaptiveTemplatePlugin):
 	meta = {
 		"Name": "FlaskTokyo",
-		"Version": "0.5",
+		"Version": "1.0",
 		"Description": "A fake captive portal based on open wireless access points in Tokyo.",
-		"Author": "@zer0-ne, @y9k8i",
+		"Author": "@y9k8i",
 		"TemplatePath": C.TEMPLATES_FLASK + "templates/FlaskTokyo",
 		"StaticPath": C.TEMPLATES_FLASK + "templates/FlaskTokyo/static",
 		"Preview": "plugins/captivePortal/templates/FlaskTokyo/preview.png",
